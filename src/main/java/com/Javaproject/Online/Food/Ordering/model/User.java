58ed1bfb1266638729c8f01b678dfeb26a41c381
anshu <anshu.rai018@gmail.com>
2024-03-10 09:37:16 +0530

    first commit

---
+++ src/main/java/com/Javaproject/Online/Food/Ordering/model/User.java
@@ -0,0 +1,41 @@
+package com.Javaproject.Online.Food.Ordering.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+
+    private String fullName;
+
+    private String email;
+
+
+    private String password;
+
+    private USER_ROLE role;
+
+    @JsonIgnore
+    @OneToMany
+    private List<Order> orders = new ArrayList<>();
+
+}
